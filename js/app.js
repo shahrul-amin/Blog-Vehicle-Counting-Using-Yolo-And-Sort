d417b6070b2062c0ce5d98e9353206a602294489
shahrul-amin <shahrulamin8776@gmail.com.com>
2024-06-22 01:58:39 +0800

    update some pages

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+  
+  menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("open");
+  };
+  
+  window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navbar.classList.remove("open");
+  };
+  
   const sr = ScrollReveal({
     distance: '60px',
-    duration: 2000,
-    delay: 200,
+    duration: 2500,
+    delay: 400,
     reset: true
-  });
-
-  sr.reveal('.banner-content, .newsletter, .blog-header', {delay: 150, origin: 'left'});
-  sr.reveal('.featured-left, .featured-right, .blog-card', {delay: 150, origin: 'top'});
+  })
+  
+  sr.reveal('.banner-content, .newsletter, .blog-header', {delay: 200, origin: 'left'})
+  sr.reveal('.featured-left, .featured-right, .blog-card', {delay: 200, origin: 'top'})
 
   const readMoreBtn = document.querySelector('.readme-btn');
   const popup = document.getElementById('popup');
